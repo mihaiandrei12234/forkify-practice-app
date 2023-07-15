b2d2440bda6e2558ffac3749a2018753dabd6d3b
mihaiandrei12234 <mihai.bastiurea@stud.acs.upb.ro>
2023-07-15 13:52:14 +0300

    Cleaned up controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -116,10 +116,6 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
-const newFeature = function () {
-  console.log('Welcome to the application');
-};
-
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -128,6 +124,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView._addHandlerUpload(controlAddRecipe);
-  newFeature();
 };
 init();
